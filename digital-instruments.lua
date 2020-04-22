6ec25c74f463474fb5265f5afb5ffab805753621
Phil Verghese <philverghese@users.noreply.github.com>
2020-04-22 14:49:46 -0700

    Show GPS course & resize

--- digital-instruments.lua
+++ digital-instruments.lua
@@ -12,22 +12,25 @@ end
 
 require('graphics')
 
+dataref('gps1_crs', 'sim/cockpit/radios/gps_course_degtm')
 dataref('nav1_obs', 'sim/cockpit/radios/nav1_obs_degm')
 dataref('nav1_fromto', 'sim/cockpit/radios/nav1_fromto')
 dataref('nav1_dme', 'sim/cockpit/radios/nav1_dme_dist_m')
+dataref('gps2_crs', 'sim/cockpit/radios/gps2_course_degtm')
 dataref('nav2_obs', 'sim/cockpit/radios/nav2_obs_degm')
 dataref('nav2_fromto', 'sim/cockpit/radios/nav2_fromto')
 dataref('nav2_dme', 'sim/cockpit/radios/nav2_dme_dist_m')
 
 local fromto_text = {[0]='OFF', [1]='TO', [2]='FROM'}
 
-local wnd = float_wnd_create(200, 80, 1, true)
+local wnd = float_wnd_create(180, 100, 1, true)
 float_wnd_set_position(wnd, 100, SCREEN_HIGHT - 150)
 float_wnd_set_title(wnd, 'Digital Instruments')
 float_wnd_set_imgui_builder(wnd, 'DIGI_build_window')
 
 function DIGI_build_window(wnd, x, y)
     imgui.TextUnformatted('        NAV1    NAV2')
+    imgui.TextUnformatted('GPS     ' .. string.format('%03d', gps1_crs) .. string.format('     %03d', gps1_crs))
     imgui.TextUnformatted('OBS     ' .. string.format('%03d', nav1_obs) .. string.format('     %03d', nav2_obs))
     imgui.TextUnformatted('FLAG    ' .. string.format('%-4s', fromto_text[nav1_fromto]) .. string.format('    %-4s', fromto_text[nav2_fromto]))
     imgui.TextUnformatted('DME     ' .. string.format('%.1f', nav1_dme) .. string.format('     %.1f', nav2_dme))
