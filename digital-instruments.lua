d545325498370ac4a36de04171c2b90c20ba9e8c
Phil Verghese <philverghese@users.noreply.github.com>
2020-04-22 14:24:10 -0700

    Nav2 works

--- digital-instruments.lua
+++ digital-instruments.lua
@@ -12,10 +12,12 @@ end
 
 require('graphics')
 
-dataref('nav1_course', 'sim/cockpit/radios/nav1_course_degm')
 dataref('nav1_obs', 'sim/cockpit/radios/nav1_obs_degm')
 dataref('nav1_fromto', 'sim/cockpit/radios/nav1_fromto')
 dataref('nav1_dme', 'sim/cockpit/radios/nav1_dme_dist_m')
+dataref('nav2_obs', 'sim/cockpit/radios/nav2_obs_degm')
+dataref('nav2_fromto', 'sim/cockpit/radios/nav2_fromto')
+dataref('nav2_dme', 'sim/cockpit/radios/nav2_dme_dist_m')
 
 local wnd = float_wnd_create(300, 100, 1, true)
 local fromto_text = {[0]='OFF', [1]='TO', [2]='FROM'}
@@ -24,10 +26,9 @@ float_wnd_set_title(wnd, 'Digital Instruments')
 float_wnd_set_imgui_builder(wnd, 'DIGI_build_window')
 
 function DIGI_build_window(wnd, x, y)
-    imgui.TextUnformatted("        NAV1    NAV2")
-    imgui.TextUnformatted("CRS     " .. string.format('%03d', nav1_course))
-    imgui.TextUnformatted("OBS     " .. string.format('%03d', nav1_obs))
-    imgui.TextUnformatted("FLAG    " .. string.format('%-4s', fromto_text[nav1_fromto]))
-    imgui.TextUnformatted("DME     " .. string.format('%.1f', nav1_dme))
-
+    imgui.TextUnformatted('        NAV1    NAV2')
+    imgui.TextUnformatted('OBS     ' .. string.format('%03d', nav1_obs) .. string.format('     %03d', nav2_obs))
+    imgui.TextUnformatted('FLAG    ' .. string.format('%-4s', fromto_text[nav1_fromto]) .. string.format('    %-4s', fromto_text[nav2_fromto]))
+    imgui.TextUnformatted('DME     ' .. string.format('%.1f', nav1_dme) .. string.format('     %.1f', nav2_dme))
 end
+
