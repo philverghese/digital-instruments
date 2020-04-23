8d97381204b112aece839814b93650d2540f695d
Phil Verghese <philverghese@users.noreply.github.com>
2020-04-23 16:11:07 -0700

    Clamp all GPS & OBS to [0, 359]

--- digital-instruments.lua
+++ digital-instruments.lua
@@ -13,15 +13,15 @@ end
 require('graphics')
 require('math')
 
-dataref('gps1_crs', 'sim/cockpit/radios/gps_course_degtm')
-dataref('nav1_obs', 'sim/cockpit/radios/nav1_obs_degm')
+dataref('gps1_crs', 'sim/cockpit/radios/gps_course_degtm', 'writable')
+dataref('nav1_obs', 'sim/cockpit/radios/nav1_obs_degm', 'writable')
 dataref('nav1_fromto', 'sim/cockpit/radios/nav1_fromto')
 dataref('nav1_dme', 'sim/cockpit/radios/nav1_dme_dist_m')
 -- The dataref docs say that "gps2_course_degtm2" means the copilot
 -- instrument. In the stock C172 with 2 GPSs, this is the one that's
 -- set for GPS2, not "gps2_course_degtm"
-dataref('gps2_crs', 'sim/cockpit/radios/gps2_course_degtm2')
-dataref('nav2_obs', 'sim/cockpit/radios/nav2_obs_degm')
+dataref('gps2_crs', 'sim/cockpit/radios/gps2_course_degtm2', 'writable')
+dataref('nav2_obs', 'sim/cockpit/radios/nav2_obs_degm', 'writable')
 dataref('nav2_fromto', 'sim/cockpit/radios/nav2_fromto')
 dataref('nav2_dme', 'sim/cockpit/radios/nav2_dme_dist_m')
 
@@ -37,9 +37,15 @@ local function clampAngle(angle)
 end
 
 function DIGI_build_window(wnd, x, y)
+    -- Some third-party instruments go outside the range [0, 359]. Clamp those.
+    gps1_crs = clampAngle(gps1_crs)
+    gps2_crs = clampAngle(gps2_crs)
+    nav1_obs = clampAngle(nav1_obs)
+    nav2_obs = clampAngle(nav2_obs)
+
     imgui.TextUnformatted('        NAV1    NAV2')
-    imgui.TextUnformatted('GPS     ' .. string.format('%03d', clampAngle(gps1_crs)) .. string.format('     %03d', clampAngle(gps2_crs)))
-    imgui.TextUnformatted('OBS     ' .. string.format('%03d', clampAngle(nav1_obs)) .. string.format('     %03d', clampAngle(nav2_obs)))
+    imgui.TextUnformatted('GPS     ' .. string.format('%03d', gps1_crs) .. string.format('     %03d', gps2_crs))
+    imgui.TextUnformatted('OBS     ' .. string.format('%03d', nav1_obs) .. string.format('     %03d', nav2_obs))
     imgui.TextUnformatted('FLAG    ' .. string.format('%-4s', fromto_text[nav1_fromto]) .. string.format('    %-4s', fromto_text[nav2_fromto]))
     imgui.TextUnformatted('DME     ' .. string.format('%.1f', nav1_dme) .. string.format('     %.1f', nav2_dme))
 end
